8eab3e0ff1fb0e403afe078154704950d172fecb
andrewreiter4 <reiterandrew04@gmail.com>
2021-08-30 20:57:50 -0700

    Initial commit

---
+++ src/SimpleATM.java
@@ -0,0 +1,31 @@
+
+import java.util.HashMap;
+
+
+public class SimpleATM 
+{   
+
+	private HashMap <String, Double > b = new HashMap <String, Double>();
+
+	public SimpleATM() {
+		
+	}
+		
+	public void deposit (String bankID, double amount)
+	{
+		if (amount > 0)
+		{
+			if (b.containsKey(bankID))
+			{
+				b.put(bankID, b.get(bankID)+ amount);
+			}
+			else
+			{
+				b.put(bankID, amount);
+			}
+		}
+	}
+
+	
+}
+
